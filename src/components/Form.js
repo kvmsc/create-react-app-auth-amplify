eff278b7fae381f2877684d2102b3a69875cf406
Mohit Chandra <kvmsc@github.com>
2020-06-27 14:29:34 +0530

    Form Component Added

---
+++ src/components/Form.js
@@ -0,0 +1,240 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { API, Auth } from "aws-amplify";
+
+export default class FormComp extends Component {
+  //Constructing props to bind 'this' to submit Handler
+  constructor(props) {
+    super(props);
+    this.state = {
+      userIdfound: false,
+      formSubmitted: false,
+      formChanged: false,
+
+      formData: {
+        user_id: "",
+        timestamp: this.props.update_data
+          ? this.props.update_data.EventTimestamp
+          : "",
+        name: this.props.update_data ? this.props.update_data.EventName : "",
+        description: this.props.update_data
+          ? this.props.update_data.EventDescription
+          : "",
+        status: this.props.update_data
+          ? this.props.update_data.EventStatus
+          : "",
+        start_date: this.props.update_data
+          ? this.props.update_data.EventSchedule.Start_time.slice(-10)
+          : "",
+        start_time: this.props.update_data
+          ? this.props.update_data.EventSchedule.Start_time.slice(0, -11)
+          : "",
+        end_date: this.props.update_data
+          ? this.props.update_data.EventSchedule.Stop_time.slice(-10)
+          : "",
+        end_time: this.props.update_data
+          ? this.props.update_data.EventSchedule.Stop_time.slice(0, -11)
+          : "",
+      },
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentWillMount = () => {
+    if (!this.state.userIdfound) {
+      Auth.currentUserInfo().then((result) => {
+        let updatedFormData = { ...this.state.formData };
+        updatedFormData.user_id = result.username;
+        this.setState({ formData: updatedFormData, userIdfound: true });
+      });
+    }
+  };
+
+  postForm = async () => {
+    try {
+      const apiName = "event-scheduler-api";
+      const path = this.props.reqPath;
+      const myInit = {
+        body: {
+          item: {
+            UserId: this.state.formData.user_id,
+            EventTimestamp: this.state.formData.timestamp,
+            EventName: this.state.formData.name,
+            EventDescription: this.state.formData.description,
+            EventStatus: this.state.formData.status,
+            EventSchedule: {
+              Start_time:
+                this.state.formData.start_time +
+                " " +
+                this.state.formData.start_date,
+              Stop_time:
+                this.state.formData.end_time +
+                " " +
+                this.state.formData.end_date,
+            },
+          },
+        },
+      };
+      console.log(myInit);
+      const response = await API.post(apiName, path, myInit);
+
+      //Logging  The response
+      console.log(response);
+
+      //the data in App State is now Stale
+      this.props.consistency.setConsistency(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  handleChange(event) {
+    //console.log(this.state.formData.name);
+    let updatedFormData = { ...this.state.formData };
+    updatedFormData[event.target.name] = event.target.value;
+    this.setState({ formData: updatedFormData, formChanged: true });
+    //console.log(this.state.formData.name);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.formChanged) {
+      this.postForm().then(() => {
+        this.setState({ formSubmitted: true });
+      });
+    } else {
+      alert("Update Form to Submit");
+    }
+  }
+
+  render() {
+    if (this.state.formSubmitted) {
+      return <Redirect to="/" />;
+    }
+
+    const {
+      user_id,
+      timestamp,
+      name,
+      description,
+      status,
+      start_date,
+      start_time,
+      end_date,
+      end_time,
+    } = this.state.formData;
+
+    return (
+      <div className="container contact-form">
+        <form onSubmit={this.handleSubmit}>
+          <h3>{this.props.formType} Event!</h3>
+          <div className="row">
+            <div className="col-12">
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={this.handleChange}
+                  placeholder="Event Name"
+                  disabled={this.props.formType === "Update"}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <textarea
+                  name="description"
+                  value={description}
+                  className="form-control"
+                  placeholder="Event Description"
+                  onChange={this.handleChange}
+                  disabled={this.props.formType === "Update"}
+                  required
+                ></textarea>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="form-group">
+                <label htmlFor="start_date">Start Date:</label>
+                <input
+                  className="form-control"
+                  type="date"
+                  name="start_date"
+                  value={start_date}
+                  onChange={this.handleChange}
+                  id="start_date"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="start_time">Start Time:</label>
+                <input
+                  className="form-control"
+                  type="time"
+                  name="start_time"
+                  value={start_time}
+                  onChange={this.handleChange}
+                  id="start_time"
+                />
+              </div>
+            </div>
+
+            <div className="col-6">
+              <div className="form-group">
+                <label htmlFor="end_date">End Date:</label>
+                <input
+                  className="form-control"
+                  type="date"
+                  name="end_date"
+                  value={end_date}
+                  onChange={this.handleChange}
+                  id="end_date"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="end_time">End Time:</label>
+                <input
+                  className="form-control"
+                  type="time"
+                  name="end_time"
+                  value={end_time}
+                  onChange={this.handleChange}
+                  id="end_time"
+                />
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="form-group">
+                <label htmlFor="select">Status:</label>
+                <select
+                  className="custom-select"
+                  name="status"
+                  value={status}
+                  onChange={this.handleChange}
+                  required
+                  id="select"
+                >
+                  <option value="">Select One</option>
+                  <option value="Idle">Idle</option>
+                  <option value="Ongoing">Ongoing</option>
+                  <option value="Finished">Finished</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <input
+                  type="submit"
+                  className="btnContact"
+                  value={this.props.formType}
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
--- src/components/UpdateEvent.js
+++ src/components/UpdateEvent.js
@@ -1,155 +1,25 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import { API } from "aws-amplify";
+import FormComp from "./Form";
 
 export default class UpdateEvent extends Component {
-  //Constructing props to bind 'this' to submit Handler
-  constructor(props) {
-    super(props);
-    this.state = {
-      formSubmitted: false,
-      injectedState: this.props.location.state,
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  eventUpdate = async () => {
-    try {
-      const apiName = "event-scheduler-api";
-      const path = "/event-update";
-      const myInit = {
-        body: {
-          item: this.state.injectedState.event,
-        }, // OPTIONAL
-      };
-      const response = await API.post(apiName, path, myInit);
-
-      //Logging  The response
-      console.log(response);
-
-      //Since Updated the data in App State is Stale
-      this.props.consistency.setConsistency(false);
-    } catch (err) {
-      console.log(err);
-    }
+  state = {
+    injectedState: this.props.location.state,
   };
 
-  //Change Handler
-  handleChange(event) {
-    let tempState = JSON.parse(JSON.stringify(this.state.injectedState));
-    //console.log(tempState);
-    tempState.event.EventStatus = event.target.value;
-    this.setState({ injectedState: tempState });
-  }
-
-  //Submit handler
-  handleSubmit(event) {
-    event.preventDefault();
-    this.eventUpdate().then(() => {
-      this.setState({ formSubmitted: true });
-    });
-  }
-
   render() {
-    //Redirect to Home page After form Submission or Illegal access
-    if (!this.state.injectedState || this.state.formSubmitted) {
+    //Redirect to Home page if Illegal access
+    if (!this.state.injectedState) {
       return <Redirect to="/" />;
     }
-    const currentEvent = this.state.injectedState.event;
+
     return (
-      <div className="container contact-form">
-        <form onSubmit={this.handleSubmit}>
-          <h3>Update Event - {currentEvent.EventName}</h3>
-          <div className="row">
-            <div className="col-7">
-              <div className="form-group">
-                <label for="start_date">Start Date:</label>
-                <input
-                  className="form-control"
-                  type="date"
-                  name="start_date"
-                  value={currentEvent.EventSchedule.Start_time.slice(-10)}
-                  onChange={this.handleChange}
-                  id="start_date"
-                />
-              </div>
-
-              <div className="form-group">
-                <label for="start_time">Start Time:</label>
-                <input
-                  className="form-control"
-                  type="time"
-                  name="start_time"
-                  value={currentEvent.EventSchedule.Start_time.slice(0, -11)}
-                  onChange={this.handleChange}
-                  id="start_time"
-                />
-              </div>
-            </div>
-
-            <div className="col-5">
-              <div className="form-group">
-                <label for="end_date">End Date:</label>
-                <input
-                  className="form-control"
-                  type="date"
-                  name="end_date"
-                  value={currentEvent.EventSchedule.Stop_time.slice(-10)}
-                  onChange={this.handleChange}
-                  id="end_date"
-                />
-              </div>
-              <div className="form-group">
-                <label for="end_time">End Time:</label>
-                <input
-                  className="form-control"
-                  type="time"
-                  name="end_time"
-                  value={currentEvent.EventSchedule.Stop_time.slice(0, -11)}
-                  onChange={this.handleChange}
-                  id="end_time"
-                />
-              </div>
-            </div>
-            <div className="col-12">
-              <div className="form-group">
-                <label for="formSelectInput">Status:</label>
-                <select
-                  id="formSelectInput"
-                  className="browser-default custom-select"
-                  name="status"
-                  value=""
-                  onChange={this.handleChange}
-                  required
-                >
-                  <option value="">Select One</option>
-                  <option
-                    value="Idle"
-                    disabled={currentEvent.EventStatus === "Idle"}
-                  >
-                    Idle
-                  </option>
-                  <option
-                    value="Ongoing"
-                    disabled={currentEvent.EventStatus === "Ongoing"}
-                  >
-                    Ongoing
-                  </option>
-                  <option
-                    value="Finished"
-                    disabled={currentEvent.EventStatus === "Finished"}
-                  >
-                    Finished
-                  </option>
-                </select>
-              </div>
-              <div className="form-group">
-                <input type="submit" className="btnContact" value="Update" />
-              </div>
-            </div>
-          </div>
-        </form>
+      <div>
+        <FormComp
+          formType="Update"
+          reqPath="/event-update"
+          update_data={this.state.injectedState.event}
+        />
       </div>
     );
   }
